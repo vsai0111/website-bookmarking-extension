218f9ccf8c8bb1b938c7a4a86b6b7ee8db8799d8
Vasamshetti sai <139035044+vsai0111@users.noreply.github.com>
2023-12-11 21:06:07 +0530

    update

--- popup.js
+++ popup.js
@@ -1,19 +1,35 @@
-document.getElementById('bookmarkButton').addEventListener('click', () => {
-    chrome.windows.getCurrent({ populate: true }, (currentWindow) => {
-      const tabs = currentWindow.tabs;
-      const bookmarks = [];
-  
-      tabs.forEach((tab) => {
-        bookmarks.push({ url: tab.url, title: tab.title });
-      });
-  
-      chrome.storage.sync.get('bookmarks', (data) => {
-        const existingBookmarks = data.bookmarks || [];
-        const updatedBookmarks = [...existingBookmarks, ...bookmarks];
-  
-        chrome.storage.sync.set({ bookmarks: updatedBookmarks }, () => {
-          alert(`Added ${tabs.length} tabs to bookmarks successfully!`);
-        });
+document.getElementById('addCurrentButton').addEventListener('click', () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const currentTab = tabs[0];
+    const currentWebsite = { url: currentTab.url, title: currentTab.title };
+
+    chrome.storage.sync.get('bookmarks', (data) => {
+      const existingBookmarks = data.bookmarks || [];
+      const updatedBookmarks = [...existingBookmarks, currentWebsite];
+
+      chrome.storage.sync.set({ bookmarks: updatedBookmarks }, () => {
+        alert(`Added current website to bookmarks successfully!`);
       });
     });
   });
+});
+
+document.getElementById('addAllTabsButton').addEventListener('click', () => {
+  chrome.windows.getCurrent({ populate: true }, (currentWindow) => {
+    const tabs = currentWindow.tabs;
+    const bookmarks = [];
+
+    tabs.forEach((tab) => {
+      bookmarks.push({ url: tab.url, title: tab.title });
+    });
+
+    chrome.storage.sync.get('bookmarks', (data) => {
+      const existingBookmarks = data.bookmarks || [];
+      const updatedBookmarks = [...existingBookmarks, ...bookmarks];
+
+      chrome.storage.sync.set({ bookmarks: updatedBookmarks }, () => {
+        alert(`Added ${tabs.length} tabs to bookmarks successfully!`);
+      });
+    });
+  });
+});
